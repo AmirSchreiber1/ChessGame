8a0af11fb8cb2d4501f08b597d1e0dc669123d55
AmirSchreiber1 <amirschreiber1@gmail.com>
2023-09-11 23:58:50 +0300

    main menu screen and set setClockTime screen are now designed and functioning properly (the color changes on the setClockTime screen are animated :))

---
+++ app/src/main/java/com/example/chessgame/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.chessgame;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    Button playButton;
+    Button clockBtn;
+    TextView welcomeMsg;
+    TextView logoTxt;
+    TextView clockTimeTV;
+    int currChoice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playButton = findViewById(R.id.playBtn);
+        clockBtn = findViewById(R.id.clockBtn);
+        welcomeMsg = findViewById(R.id.welcomeMsg);
+        logoTxt = findViewById(R.id.logoTxt);
+        clockTimeTV = findViewById(R.id.clockTime);
+        setFonts();
+        currChoice = 3;
+        clockBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SetClockActivity.class);
+            intent.putExtra("currChoice", currChoice);
+            startActivityForResult(intent, 2);
+        });
+    }
+
+    private void setFonts(){
+        Typeface mainFont = Typeface.createFromAsset(getAssets(), "font/Farmhouse.otf");
+        Typeface welcomeFont = Typeface.createFromAsset(getAssets(), "font/Writer.otf");
+        playButton.setTypeface(mainFont);
+        clockBtn.setTypeface(mainFont);
+        welcomeMsg.setTypeface(welcomeFont);
+        logoTxt.setTypeface(mainFont);
+        clockTimeTV.setTypeface(mainFont);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            currChoice=data.getIntExtra("clockTimeChoice", 3);
+            String clockTimeStr = null;
+            switch(currChoice) {
+                case 1:
+                    clockTimeStr = "Clock Time: 02:00";
+                    break;
+                case 2:
+                    clockTimeStr = "Clock Time: 05:00";
+                    break;
+                case 3:
+                    clockTimeStr = "Clock Time: 10:00";
+                    break;
+                case 4:
+                    clockTimeStr = "Clock Time: unlimited";
+                    break;
+            }
+            clockTimeTV.setText(clockTimeStr);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chessgame/SetClockActivity.java
@@ -0,0 +1,176 @@
+package com.example.chessgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SetClockActivity extends AppCompatActivity {
+    TextView bulletSymbol;
+    TextView bulletTime;
+    TextView lightningSymbol;
+    TextView blitzTime;
+    TextView clockSymbol;
+    TextView rapidTime;
+    TextView infinitySymbol;
+    TextView unlimitedTime;
+    LinearLayout bulletCell;
+    LinearLayout blitzCell;
+    LinearLayout rapidCell;
+    LinearLayout unlimitedCell;
+    Button saveButton;
+    int currChoice; //1=bullet 2=blitz 3=rapid 4=unlimited
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_clock);
+        bulletSymbol = findViewById(R.id.bullet);
+        bulletTime = findViewById(R.id.bullet_time);
+        lightningSymbol = findViewById(R.id.lightning);
+        blitzTime = findViewById(R.id.blitz_time);
+        clockSymbol = findViewById(R.id.clock);
+        rapidTime = findViewById(R.id.rapid_time);
+        infinitySymbol = findViewById(R.id.infinity);
+        unlimitedTime = findViewById(R.id.unlimited_time);
+        saveButton = findViewById(R.id.save_button);
+        setFonts();
+        bulletCell = findViewById(R.id.bullet_cell);
+        blitzCell = findViewById(R.id.blitz_cell);
+        rapidCell = findViewById(R.id.rapid_cell);
+        unlimitedCell = findViewById(R.id.unlimited_cell);
+        //set background color for pre-chosen (or default if no change previously) clockTime cell:
+        setPreChosenCellColor();
+        //set onClickListeners for the 4 options displayed on screen:
+        setOnClickListeners();
+    }
+
+    private void setFonts() {
+        Typeface mainFont = Typeface.createFromAsset(getAssets(), "font/Farmhouse.otf");
+        Typeface bulletFont = Typeface.createFromAsset(getAssets(), "font/Special Forces.ttf");
+        Typeface lightning_clock_font = Typeface.createFromAsset(getAssets(), "font/Icons South St.ttf");
+        bulletTime.setTypeface(mainFont);
+        blitzTime.setTypeface(mainFont);
+        rapidTime.setTypeface(mainFont);
+        unlimitedTime.setTypeface(mainFont);
+        bulletSymbol.setTypeface(bulletFont);
+        lightningSymbol.setTypeface(lightning_clock_font);
+        clockSymbol.setTypeface(lightning_clock_font);
+        saveButton.setTypeface(mainFont);
+    }
+
+    private void setPreChosenCellColor() {
+        LinearLayout chosenCell = null;
+        Intent intent = getIntent();
+        currChoice = intent.getIntExtra("currChoice", 2);
+        switch (currChoice) {
+            case 1:
+                chosenCell = bulletCell;
+                break;
+            case 2:
+                chosenCell = blitzCell;
+                break;
+            case 3:
+                chosenCell = rapidCell;
+                break;
+            case 4:
+                chosenCell = unlimitedCell;
+                break;
+        }
+        chosenCell.setBackgroundColor(getColor(R.color.light_gray));
+    }
+
+    private void setOnClickListeners() {
+        bulletCell.setOnClickListener(view -> {
+            changeChoice(1);
+        });
+        blitzCell.setOnClickListener(view -> {
+            changeChoice(2);
+        });
+        rapidCell.setOnClickListener(view -> {
+            changeChoice(3);
+        });
+        unlimitedCell.setOnClickListener(view -> {
+            changeChoice(4);
+        });
+        saveButton.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.putExtra("clockTimeChoice", currChoice);
+            setResult(2, intent);
+            finish();
+        });
+    }
+
+    private void changeChoice(int newChoice) {
+        if (currChoice == newChoice) return;
+        LinearLayout toWhiteCell = null;
+        LinearLayout toGrayCell = null;
+        switch (currChoice) {
+            case 1:
+                toWhiteCell = bulletCell;
+                break;
+            case 2:
+                toWhiteCell = blitzCell;
+                break;
+            case 3:
+                toWhiteCell = rapidCell;
+                break;
+            case 4:
+                toWhiteCell = unlimitedCell;
+                break;
+        }
+
+        switch (newChoice) {
+            case 1:
+                toGrayCell = bulletCell;
+                break;
+            case 2:
+                toGrayCell = blitzCell;
+                break;
+            case 3:
+                toGrayCell = rapidCell;
+                break;
+            case 4:
+                toGrayCell = unlimitedCell;
+                break;
+        }
+
+        changeToWhite(toWhiteCell);
+        changeToGray(toGrayCell);
+        currChoice = newChoice;
+    }
+
+    private void changeToWhite(LinearLayout toWhiteCell) {
+        int colorFrom = getColor(R.color.light_gray);
+        int colorTo = getColor(R.color.white);
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(100); // milliseconds
+        colorAnimation.addUpdateListener(animator -> toWhiteCell.setBackgroundColor((int) animator.getAnimatedValue()));
+        colorAnimation.start();
+    }
+
+    private void changeToGray(LinearLayout toGrayCell) {
+        int colorFrom = getColor(R.color.white);
+        int colorTo = getColor(R.color.light_gray);
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(150); // milliseconds
+        colorAnimation.addUpdateListener(animator -> toGrayCell.setBackgroundColor((int) animator.getAnimatedValue()));
+        colorAnimation.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("clockTimeChoice", currChoice);
+        setResult(2, intent);
+        super.onBackPressed();
+    }
+}
